bdda4bba5dbb7a564ae5b05a4baaca0acf432333
Indra Mahesa <indramahesa128@gmail.com>
2026-09-17 16:23:09 +0700

    feat(docs): update API overview and events docs

--- src/content/api/_meta.js
+++ src/content/api/_meta.js
@@ -1,5 +1,8 @@
 export default {
   overview: "Overview",
+  authentication: "Authentication",
+  quickstart: "Quickstart",
+  "decode-api": "Decode API",
   "events-api": "Events",
   "contracts-api": "Contracts",
 };
